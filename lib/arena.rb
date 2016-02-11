0a4d7f99d6b1b18aa680d00cb9059d1c52173864
Sam Phippen <samphippen@googlemail.com>
2016-02-11 19:53:49 +0000

    Add a partially functional game of life impl

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,2 +1,58 @@
 class Arena
+  def initialize(world)
+    @world = world
+  end
+
+  def step
+    @new_world = world.dup.map! { |row| row.dup }
+    world.each_with_index do |row_array, row_position|
+      row_array.each_with_index do |cell, column_position|
+        an = alive_neighbors(column_position, row_position)
+        if cell && an  < 2
+          kill(column_position, row_position)
+        elsif cell && an > 3
+          kill(column_position, row_position)
+        elsif cell && [2,3].include?(an)
+          #!?!?!?!?!
+        elsif !cell && an == 3
+          bring_to_life(column_position, row_position)
+        end
+      end
+    end
+
+    @world = new_world
+
+    world
+  end
+
+  def alive_neighbors(col, row)
+    count = 0
+    (-1..1).each do |y_offset|
+      (-1..1).each do |x_offset|
+        if !(y_offset == 0 && x_offset == 0)
+          if (col + x_offset >= 0) && (row + y_offset >= 0)
+            if world[row+y_offset] && world[row+y_offset][col+x_offset]
+              count += 1
+            end
+          end
+        end
+      end
+    end
+
+    count
+  end
+
+  protected
+
+  attr_reader :world, :new_world
+
+  private
+
+  def kill(col, row)
+    new_world[row][col] = false
+  end
+
+  def bring_to_life(col, row)
+    new_world[row][col] = true
+  end
 end
--- spec/unit/arena_spec.rb
+++ spec/unit/arena_spec.rb
@@ -2,7 +2,120 @@ require 'spec_helper'
 require 'arena.rb'
 
 RSpec.describe Arena do
-  it 'can be constructed' do
-    expect(described_class.new).not_to be_nil
+  describe "#step" do
+    it "does the correct thing with the column example" do
+      new_grid = Arena.new([
+        [false, true, false],
+        [false, true, false],
+        [false, true, false],
+      ]).step
+
+      expect(new_grid).to eq([
+        [false, false, false],
+        [true, true, true],
+        [false, false, false],
+      ])
+    end
+
+    it "retains stable patterns" do
+      world = [
+        [false, true, false],
+        [true, false, true],
+        [false, true, false],
+      ]
+
+      new_grid = Arena.new(world).step
+
+      expect(new_grid).to eq(world)
+    end
+
+    it "kills a lonley cell" do
+      world = [
+        [false, true, false],
+        [false, false, false],
+        [false, false, false],
+      ]
+
+      new_grid = Arena.new(world).step
+
+      expect(new_grid).to eq([
+        [false, false, false],
+        [false, false, false],
+        [false, false, false],
+      ])
+    end
+
+    it "keeps the square shape stable" do
+      world = [
+        [true, true, false],
+        [true, true, false],
+        [false, false, false],
+      ]
+
+      new_grid = Arena.new(world).step
+
+      expect(new_grid).to eq([
+        [true, true, false],
+        [true, true, false],
+        [false, false, false],
+      ])
+    end
+  end
+
+  describe "#alive_neighbors" do
+    it "returns 0 for a cell with 0 neighbors" do
+      world = [
+        [false, false, false],
+        [false, true, false],
+        [false, false, false],
+      ]
+
+      arena = Arena.new(world)
+      expect(arena.alive_neighbors(1,1)).to be 0
+    end
+
+    it "returns 1 for a cell with 1 neighbors" do
+      world = [
+        [true, false, false],
+        [true, false, false],
+        [false, false, false],
+      ]
+
+      arena = Arena.new(world)
+      expect(arena.alive_neighbors(0,0)).to be 1
+    end
+
+    it "returns 4 for a cell with 4 diagonal neighbors" do
+      world = [
+        [true, false, true],
+        [false, false, false],
+        [true, false, true],
+      ]
+
+      arena = Arena.new(world)
+      expect(arena.alive_neighbors(1,1)).to be 4
+    end
+
+    it "returns 2 for a cell with 2 diagonal neighbors" do
+      world = [
+        [false, true, false],
+        [false, false, false],
+        [false, false, true],
+      ]
+
+      arena = Arena.new(world)
+      expect(arena.alive_neighbors(2,1)).to be 2
+    end
+
+    it "returns 1 for a cell at the top of the column" do
+      world = [
+        [false, true, false],
+        [false, true, false],
+        [false, true, false],
+      ]
+
+      arena = Arena.new(world)
+      expect(arena.alive_neighbors(1,2)).to be 1
+    end
   end
 end
